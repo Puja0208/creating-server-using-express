10483ae05fcdefc44ff95c58b6282b6637bf0a5b
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-04-26 06:12:08 +0530

    feat: Handling POST requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const courses = [
   {
@@ -27,6 +28,19 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === +req.params.id);
+  if (!course) {
+    //404 -object not found
+    res.status(404).send("Course with given id not found");
+  }
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 //PORT
